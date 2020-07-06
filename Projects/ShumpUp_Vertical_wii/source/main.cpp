4c6037ec125be2a8d41402fa24f9a443cc4b6f31
luiz.nai <luiz.nai@afferolab.com.br>
2020-07-06 01:39:24 -0300

    Adding ShumUp Vertical tempalte for the Nintendo Wii.

---
+++ Projects/ShumpUp_Vertical_wii/source/main.cpp
@@ -0,0 +1,11 @@
+/*Code and engine made by Titan Game Studios 2016/2017 coded by Luiz Nai.*/
+#include "game.h"
+
+
+/////General function of the game
+int main(int argc, char** argv) 
+{
+	game g;
+	g.start();
+	return 0;
+}

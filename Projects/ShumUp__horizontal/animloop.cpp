582b064e50b65c464b043bda3c36c4accb01ff05
Luiz Nai <luiz.neosnk@gmail.com>
2021-02-08 00:28:53 -0300

    Horizontal ShumpUp with new analog code, new VMU, Rumble Pack, Animate fonts and animate classes added.

---
+++ Projects/ShumUp__horizontal/animloop.cpp
@@ -0,0 +1,52 @@
+/*Code and engine made by Titan Game Studios 2016/2021 coded by Luiz Nai.*/
+#include "animloop.h"
+
+/////Main function for the enemy
+animloop::animloop(SDL_Surface* img, int x, int y, int item_size, double total_frames, bool running, int frame_speed)
+{
+    speed = frame_speed;
+    count_frames = 0;
+	image=img;
+    t_frames = total_frames;
+    box.x=x;
+    box.y=y;
+    box.w=item_size;
+    box.h=item_size;
+    is_running=running;
+
+	for (int i=0;i<t_frames;i++)
+	{
+		clips[i].x=i*item_size;
+		clips[i].y=0;
+		clips[i].w=item_size;
+		clips[i].h=item_size;
+	}
+    
+	frame=0;
+}
+
+///// Move the item
+void animloop::move()
+{
+    count_frames++;
+    if(is_running && count_frames==speed) {
+        frame+=0.1;
+        if(frame>t_frames) {
+            frame = 0.0;
+        }
+    }
+    else if(count_frames>speed) {
+        count_frames=0;
+    }
+}
+
+//// Show the item on the screen
+void animloop::show(SDL_Surface* screen)
+{
+    SDL_Rect tmp={box.x,box.y,40,40};
+	SDL_BlitSurface(image,&clips[(int)(frame)],screen,&tmp);
+}
+
+void animloop::setRunning(bool running) {
+    is_running = running;
+}
--- Projects/ShumUp__horizontal/game.cpp
+++ Projects/ShumUp__horizontal/game.cpp
@@ -30,6 +30,8 @@ game::game() //constructor
 
 	joystick = SDL_JoystickOpen(0);
 	buttonCount = SDL_JoystickNumButtons(joystick);
+	my_vmu = new vmu();
+	rumble_pack = new rumble(true);
 
 	load_assets(0);
 	is_shoting = false;
@@ -252,6 +254,7 @@ SDL_Surface *game::load_image(const char *filename, const char* extension, int r
 void game::handleEvents()
 {
 	SDL_Event event;
+	int limit = 25;
 
 	while (SDL_PollEvent(&event))
 	{
@@ -280,52 +283,44 @@ void game::handleEvents()
 			break;
 			
 			case SDL_JOYAXISMOTION:
-				 
-				if(event.jaxis.value!=0) 
-				{	
-					if( event.jaxis.axis==0 ){ //left right
-						axi_X=event.jaxis.value;
-					}
-					else{ //up down
-						axi_Y=event.jaxis.value;
-					}
 					
-					int limit = 50;
-
-					if(axi_Y < -limit && axi_X < -limit) {
-						player1->setDirection('q');
-					}
-					else if(axi_Y < -limit && axi_X > limit){
-						player1->setDirection('e');
-					}
-					else if(axi_Y > limit && axi_X > limit) { 
-						player1->setDirection('c');
-					}
-					else if(axi_Y > limit && axi_X < -limit){
-						player1->setDirection('x');
-					}
-					
-					if(axi_X < 0 && axi_Y >-limit && axi_Y <limit){
-						player1->setDirection('l');
-					}
-					else if(axi_X > 0 && axi_Y >-limit && axi_Y <limit) {
-						player1->setDirection('r');
-					}
-					
-					
-					if(axi_Y < 0 && axi_X > -limit && axi_X < limit){
-						player1->setDirection('u');
-					}
-					else if(axi_Y > 0 && axi_X > -limit && axi_X < limit){
-						player1->setDirection('d');
-					}
+				if( event.jaxis.axis==0 ){ //left right
+					axi_X=event.jaxis.value;
+				}
+						
+				if( event.jaxis.axis==1 ){ //up down
+					axi_Y=event.jaxis.value;
+				}
+						
+				if(axi_Y < -limit && axi_X < -limit) {
+					player1->setDirection('q');
+				}
+				else if(axi_Y < -limit && axi_X > limit){
+					player1->setDirection('e');
+				}
+				else if(axi_Y > limit && axi_X > limit) { 
+					player1->setDirection('c');
+				}
+				else if(axi_Y > limit && axi_X < -limit){
+					player1->setDirection('x');
+				}
+				else if(axi_X < 0 && axi_Y >-limit && axi_Y <limit){
+					player1->setDirection('l');
+				}
+				else if(axi_X > 0 && axi_Y >-limit && axi_Y <limit) {
+					player1->setDirection('r');
+				}
+				else if(axi_Y < 0 && axi_X > -limit && axi_X < limit){
+					player1->setDirection('u');
+				}
+				else if(axi_Y > 0 && axi_X > -limit && axi_X < limit){
+					player1->setDirection('d');
 				}
 				else {
 					axi_X = 0;
 					axi_Y = 0;	
 					player1->setDirection('z');
-				}
-								
+				}								
 			break;
 
 			case SDL_JOYHATMOTION:
@@ -694,7 +689,7 @@ void game::update_screen() {
 void game::start()
 {
 	Uint32 start;
-	vmu();
+	my_vmu->draw(1);
 	player1->setDirection('z');
 
 	while (all_running)
